2d60d6716479cfa0b881c6a936e8bbeaf36c8751
eadogan <e.ahmetdogan@gmail.com>
2017-09-30 02:22:18 +0100

    Project Created

---
+++ https:/github.com/eadogan/NoteCalculate.git/NoteCalculate/src/main/java/mxapplab/NoteCalculate/App.java
@@ -0,0 +1,13 @@
+package mxapplab.NoteCalculate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
