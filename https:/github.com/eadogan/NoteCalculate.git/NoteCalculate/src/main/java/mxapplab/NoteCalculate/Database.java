a575cc31c4123848995801c7aa075f0daff39f6b
eadogan <e.ahmetdogan@gmail.com>
2017-09-30 03:10:42 +0100

    DB

---
+++ https:/github.com/eadogan/NoteCalculate.git/NoteCalculate/src/main/java/mxapplab/NoteCalculate/Database.java
@@ -0,0 +1,11 @@
+package mxapplab.NoteCalculate;
+
+public interface Database {
+	
+	//Normal Note Calculate
+	public boolean calculateNormally(Student student);
+	
+	//Average Note Calculate
+	public boolean calculateAverage(Student student);
+
+}
